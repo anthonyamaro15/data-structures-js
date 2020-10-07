0ba19084d5d476fe9f5311bf88453d2d55b4516d
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-06 17:48:30 -0700

    binary search with javascript

---
+++ app.js
@@ -0,0 +1,18 @@
+// binary search
+function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  while (arr[middle] !== value && left <= right) {
+    if (value < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+  return arr[middle] === value ? middle : -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 40));
