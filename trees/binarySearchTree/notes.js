feee73c628a3eb234e4fca2c31092b0efa2e8a94
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-03-04 22:03:25 -0800

    feat(BST): differences between trees

---
+++ trees/binarySearchTree/notes.js
@@ -0,0 +1,4 @@
+// The difference between a balance and unbalanced BST is that the lookup, insert, delete operations for 
+// a balanced BST is going to be O(log n)
+// for an unbalanced BST is going to be O(n) 
+// because it almost becames like a Linked List
