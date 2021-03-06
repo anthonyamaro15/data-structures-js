ca31b371d85a776349e64bd04c34596682be7c1c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-03-05 20:22:57 -0800

    feat(graphs): explain different types of graphs

---
+++ graphs/graph.js
@@ -0,0 +1,29 @@
+// UNDIRECTED GRAPH
+
+// A good way to think about a directed graph is to think of as a highway between two cities where you
+// can go back and forth
+
+// think of it as a doubly linked list, where you can go both ways with then next and previous 
+
+
+
+
+// DIRECTED GRAPH
+
+// a directed graph will only be one way street where you can only go one way 
+// think of it as a linked list where you can only go one way with the next 
+
+
+
+// Weighted Graphs
+
+// UNWEIGHTED
+
+
+
+// WEIGHETED
+
+// with weighted graphs you can have informations in the edges and the connetions maybe you're going on a trip
+// and trying to figure out the most effecient way to visit sites that interest you
+// Google maps would use a weighted graph to decided what is the shortest path
+// to get there and these sort of grapsh are used a lot in calculating optiomal paths
--- trees/binarySearchTree/notes.js
+++ trees/binarySearchTree/notes.js
@@ -1,4 +1,11 @@
+//    BALANCED BST | UNBALANCED BST
 // The difference between a balance and unbalanced BST is that the lookup, insert, delete operations for 
 // a balanced BST is going to be O(log n)
 // for an unbalanced BST is going to be O(n) 
 // because it almost becames like a Linked List
+
+
+
+
+//   BINARY HEAPS
+// Binary Heaps are really good at doing copetitive operations
